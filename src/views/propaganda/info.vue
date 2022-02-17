95eb72f010661145b763ab8970e15af788987c8e
KuShuZhen <406499401@qq.com>
2022-02-17 16:38:42 +0800

    宣传详情页面完成

--- src/views/propaganda/info.vue
+++ src/views/propaganda/info.vue
@@ -13,15 +13,113 @@
 
     </div>
     <div class="info-update-time">
-      编辑于{{ info.updateTime }}
+      编辑于 {{ info.updateTime }}
     </div>
     <div class="info-label">
-      <el-tag>{{
+      <el-tag type="success" class="info-label-item">{{
           associationType.find(function (value) {
             return value.value === info.associationType;
           }).label
         }}
       </el-tag>
+      <el-tag type="success" class="info-label-item">
+        宣传
+      </el-tag>
+      <el-tag type="success" class="info-label-item">
+        流浪动物
+      </el-tag>
+    </div>
+    <div class="info-function">
+      <div class="info-function-up info-function-item"
+           @click="addUsers"
+           title="点赞后就不能取消了哦~">
+        <svg-icon :icon-class="isUp?'up-yes':'up-no'" class="info-function-up-svg info-function-svg"/>
+        <span :class="{green:isUp}" class="info-function-up-text info-function-text">
+              {{ info.user !== null && info.user !== undefined ? info.user.length : 0 }}个喜欢
+        </span>
+      </div>
+      <div class="info-function-comment info-function-item"
+           @click="isShowComment = !isShowComment">
+        <svg-icon icon-class="comment-hui" class="info-function-comment-svg info-function-svg"/>
+        <span class="info-function-comment-text info-function-text">
+          {{
+            isShowComment ? '收起评论' : '查看' + (info.comment !== null && info.comment !== undefined ? info.comment.length : 0) + '条评论'
+          }}
+        </span>
+      </div>
+    </div>
+
+    <div class="info-comment" v-if="isShowComment&&info.comment">
+      <div class="info-comment-num">
+        {{ info.comment !== null && info.comment !== undefined ? info.comment.length : 0 }}条评论
+      </div>
+      <div class="info-comment-for">
+        <div class="info-comment-item" v-for="comment in info.comment">
+          <div class="info-comment-left">
+            <div class="info-comment-item-photo">
+              <el-image
+                v-if="comment.createUserPhoto"
+                class="info-comment-item-photo-img"
+                :src="comment.createUserPhoto"
+                fit="cover"></el-image>
+            </div>
+          </div>
+          <div class="info-comment-right">
+            <div class="info-comment-item-name-time">
+              <span class="info-comment-item-name">{{ comment.createUserName }}</span>
+              <span class="info-comment-item-time">
+            {{ $moment(comment.createTime).format('YYYY-MM-DD') }}</span>
+            </div>
+            <div class="info-comment-item-content">
+              {{ comment.content }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="info-comment-add">
+        <div class="info-comment-add-left">
+          <el-input
+            class="info-comment-add-input"
+            placeholder="请输入你的评论"
+            v-model="comment"
+            clearable>
+          </el-input>
+        </div>
+        <div class="info-comment-add-right">
+          <el-button
+            class="info-comment-add-button"
+            :disabled="comment === ''"
+            @click="addComments"
+            type="primary">发布
+          </el-button>
+        </div>
+
+      </div>
+    </div>
+    <div class="info-comment" v-if="isShowComment&&!info.comment">
+      <div class="info-comment-num">
+        暂无评论
+      </div>
+
+      <div class="info-comment-add">
+        <div class="info-comment-add-left">
+          <el-input
+            class="info-comment-add-input"
+            placeholder="请输入你的评论"
+            v-model="comment"
+            clearable>
+          </el-input>
+        </div>
+        <div class="info-comment-add-right">
+          <el-button
+            class="info-comment-add-button"
+            :disabled="comment === ''"
+            @click="addComments"
+            type="primary">发布
+          </el-button>
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
@@ -57,7 +155,7 @@ export default {
       //当前用户是否评论该文章
       isComment: false,
       //当前用户发表的评论
-      comment: '测试评论',
+      comment: '',
       // 关联类型选择
       associationType: [
         {
@@ -73,6 +171,8 @@ export default {
           label: '资源对接'
         }
       ],
+      //是否展示评论
+      isShowComment: false,
     }
   },
   methods: {
@@ -125,31 +225,186 @@ export default {
     },
     //点赞文章
     addUsers() {
-      this.listLoading = true;
-      let param = {
-        id: this.infoId,
-        user: [{
-          id: this.id
-        }]
-      }
-      addUser(param).then(response => {
-        if (response.code === 200 && response.data) {
-          this.isUp = true
-        } else {
-          this.$message({
-            message: '发生错误!',
-            type: 'error'
-          });
+      if (!this.isUp) {
+        this.listLoading = true;
+        let param = {
+          id: this.infoId,
+          user: [{
+            id: this.id
+          }]
         }
-      }).finally(() => {
-        this.getPropaganda(this.infoId)
-        this.listLoading = false;
-      })
+        addUser(param).then(response => {
+          if (response.code === 200 && response.data) {
+            this.isUp = true
+          } else {
+            this.$message({
+              message: '发生错误!',
+              type: 'error'
+            });
+          }
+        }).finally(() => {
+          this.getPropaganda(this.infoId)
+          this.listLoading = false;
+        })
+      }
     },
   }
 }
 </script>
 
 <style scoped lang="scss">
+.info-body {
+  margin: 1.5em 5em;
 
+  .info-title {
+    font-size: 2em;
+    font-weight: bolder;
+    margin-bottom: 0.5em;
+    line-height: 2em;
+  }
+
+  .info-author {
+    font-size: 1em;
+    font-weight: bolder;
+    margin-bottom: 0.5em;
+    line-height: 1em;
+  }
+
+  .info-zanNum {
+    font-size: 0.75em;
+    margin-bottom: 0.5em;
+    color: #5a5e66;
+  }
+
+  .info-main {
+    width: 100%;
+    overflow-x: hidden;
+    margin-bottom: 2em;
+  }
+
+  .info-update-time {
+    font-size: 1em;
+    margin-bottom: 1em;
+    color: #5a5e66;
+  }
+
+  .info-label {
+    .info-label-item {
+      margin-right: 1em;
+    }
+  }
+
+  .info-function {
+    margin-top: 2em;
+    font-size: 1em;
+
+    .info-function-item {
+      display: inline-block;
+      margin-right: 2em;
+    }
+
+    .info-function-svg {
+
+    }
+
+    .info-function-text {
+
+    }
+  }
+
+  .info-comment {
+    margin-top: 2em;
+    box-shadow: 1px 1px 5px #888888;
+
+    .info-comment-num {
+      font-size: 1em;
+      font-weight: bolder;
+      padding: 1em;
+      border-bottom: 0.5px solid rgba(136, 136, 136, 0.30);
+    }
+
+    .info-comment-for {
+      max-height: 50vh;
+      overflow-y: auto;
+
+      .info-comment-item {
+        padding: 1em 1em 0;
+        border-top: 0.5px solid rgba(136, 136, 136, 0.30);
+        font-size: 1em;
+        line-height: 2em;
+
+        .info-comment-left {
+          display: inline-block;
+          vertical-align: top;
+          width: 5%;
+
+          .info-comment-item-photo {
+
+            .info-comment-item-photo-img {
+              width: 2em;
+              height: 2em;
+            }
+          }
+        }
+
+        .info-comment-right {
+          display: inline-block;
+          width: 95%;
+
+          .info-comment-item-name-time {
+            vertical-align: top;
+
+
+            .info-comment-item-name {
+
+            }
+
+            .info-comment-item-time {
+              float: right;
+              font-size: 0.9em;
+              color: #8c939d;
+            }
+          }
+
+          .info-comment-item-content {
+            display: -webkit-box;
+            overflow: hidden;
+            white-space: normal !important;
+            text-overflow: ellipsis;
+            word-wrap: break-word;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+          }
+        }
+
+
+      }
+    }
+
+
+    .info-comment-add {
+      border-top: 0.5px solid rgba(136, 136, 136, 0.30);
+
+      .info-comment-add-left {
+        width: 90%;
+        display: inline-block;
+        padding: 1em;
+
+        .info-comment-add-input {
+
+        }
+      }
+
+      .info-comment-add-right {
+        width: 10%;
+        display: inline-block;
+        padding: 1em;
+
+        .info-comment-add-button {
+
+        }
+      }
+    }
+  }
+}
 </style>
